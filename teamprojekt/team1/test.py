9a35c6df5eb393b144cbdc04b4800b3819bfc7dd
fabian <ebelf@hochschule-trier.de>
2016-01-11 14:36:41 +0100

    new

---
+++ teamprojekt/team1/test.py
@@ -0,0 +1,19 @@
+'''
+Created on 11.01.2016
+
+@author: fabian
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        3+4+5+6
+        
+        
