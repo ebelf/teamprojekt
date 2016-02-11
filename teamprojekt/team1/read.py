7794e5b069dda7736f68f471fd51919972d892af
fabian <ebelf@hochschule-trier.de>
2016-02-11 10:52:15 +0100

    ..

--- teamprojekt/team1/read.py
+++ teamprojekt/team1/read.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
+inputfile = "input.xml"
+
+
 Pst = []
 Dst = []
 
 import datetime
 
-from xml.dom.minidom import parse
 import xml.dom.minidom
 
 # Open XML document using minidom parser
-DOMTree = xml.dom.minidom.parse("input.xml")
+DOMTree = xml.dom.minidom.parse(inputfile)
 collection = DOMTree.documentElement
 
 # Get all the students in the collection
@@ -26,7 +28,6 @@ for student in students:
         Seminar.append(int(seminar.childNodes[0].data))
         Std.append(Seminar)
     Pst.append(Std)
-    #print(student.getElementsByTagName("year")[0].childNodes[0].data)
     date = datetime.datetime(int(student.getElementsByTagName("year")[0].childNodes[0].data),int(student.getElementsByTagName("month")[0].childNodes[0].data),int(student.getElementsByTagName("day")[0].childNodes[0].data),int(student.getElementsByTagName("hour")[0].childNodes[0].data),int(student.getElementsByTagName("minute")[0].childNodes[0].data),int(student.getElementsByTagName("second")[0].childNodes[0].data))
     Dst1.append(date)
     Dst1.append(student.getElementsByTagName("pflichtanmeldung")[0].childNodes[0].data)
