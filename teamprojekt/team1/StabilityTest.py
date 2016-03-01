21a9620b6917a47a9031b7526a05be177b77926a
marc <marc@192.168.2.104>
2016-03-01 19:01:03 +0100

    Test auf stable matching, Fehler verbessert

--- teamprojekt/team1/StabilityTest.py
+++ teamprojekt/team1/StabilityTest.py
@@ -1,11 +1,13 @@
 from readXML import PSt, DSt
-
 # import datetime
 # Test ob das Ergebnis ein stabiles Matching ist
 
 #candidate = [{0, 9, 5, 6}, {2, 3}, {1, 4}]
 #candidate = [{1, 9, 5, 6}, {2, 3}, {0, 4}] # Testkandidat mit blockierenden Paaren
 
+#candidate = [{1, 2, 3, 5, 6, 7, 13, 15, 16, 17}, {8, 9, 11, 18, 19}, {0, 4, 10, 12, 14}]
+
+#candidate = [{0, 2, 4, 5, 7}, {3, 6}, {9}]
 
 # PSt = [[[3, 1], [1, 2], [2, 3]], [[1, 1], [2, 2], [3, 3]], [[3, 1], [1, 2], [2, 3]], [[1, 1], [3, 2], [2, 3]], [[2, 1], [1, 2], [3, 3]], [[3, 1], [1, 2], [2, 3]], [[3, 1], [2, 2], [1, 3]], [[2, 1], [1, 2], [3, 3]], [[3, 1], [1, 2], [2, 3]], [[2, 1], [1, 2], [3, 3]]]
 # DSt = [[datetime.datetime(2015, 3, 22, 12, 20, 23), 'PA'], [datetime.datetime(2015, 3, 19, 16, 1, 35), 'PA'], [datetime.datetime(2015, 2, 3, 12, 42, 5), 'NA'], [datetime.datetime(2015, 2, 22, 11, 39, 2), 'PA'], [datetime.datetime(2015, 2, 1, 17, 19, 48), 'PA'], [datetime.datetime(2015, 2, 19, 11, 4, 49), 'PA'], [datetime.datetime(2015, 2, 23, 17, 52, 23), 'PA'], [datetime.datetime(2015, 3, 15, 16, 19, 3), 'NA'], [datetime.datetime(2015, 3, 26, 16, 1, 57), 'NA'], [datetime.datetime(2015, 3, 26, 9, 17, 9), 'PA']]
@@ -33,15 +35,15 @@ def stabilityCheck(candidate):
                                 break                                      
                             if 'PA' not in DSt[j] and 'PA' in DSt[z]:
                                 pass
-                            else:               # zu pruefender Student konnte durch Pflichtanmeldung keinen Vorzug erhalten --> Ueberpruefen wer die hoehere Praeferenz fuer das jeweilige Seminar hat
-                                for v in PSt[z]:    # Prioritaeten des Studenten im Matching-Ergebnis durchgehen
+                            else:               # zu pruefender Student und Student im Matching-Ergebnis sind beide PA oder beide NA --> Ueberpruefen wer die hoehere Praeferenz fuer das jeweilige Seminar hat
+                                for v in PSt[z]:    # alle Prioritaeten des Studenten im Matching-Ergebnis durchgehen
                                     if v[1]==k[1]:  # wenn gleiches Seminar
                                         testpref = v[0]         # Praeferenz in testpref speichern
                                         if k[0] > testpref:     # wenn die Praeferenz des zu pruefenden Studenten hoeher ist als die des sich im Seminar befindenden Studenten im Matching-Ergebnis
                                             blockingpairs+=1
                                             print("    blockierendes Paar bei Student", j , "mit Sem", k[1], "bei", z, "wegen PR")
                                             break
-                                        else:       # zu pruefender Student hatte keine hoehere Praeferenz fuer das Seminar und konnte dadurch keinen Vorzug erhalten, daher Anmeldezeitpunkt ueberpruefen
+                                        elif k[0] == testpref:          # zu pruefender Student und Student im Matching-Ergebnis haben gleiche Praeferenz fuer das Seminar, daher Anmeldezeitpunkt ueberpruefen
                                             if DSt[j][0]<DSt[z][0]:     # wenn der Anmeldezeitpunkt des zu ueberpr. Studenten vor dem des sich im Seminar befindenden Studenten liegt
                                                 blockingpairs+=1
                                                 print("    blockierendes Paar bei Student", j , "mit Sem", k[1],"bei",z, "wegen TS", )
