205cd59cef7d65ac22c9d02f9c74f3d46b151e89
marc <marc@192.168.2.104>
2016-03-04 00:36:03 +0100

    weitere Funktionen eingebunden

--- teamprojekt/team1/start.py
+++ teamprojekt/team1/start.py
@@ -1,10 +1,16 @@
 # Datei zum Ausfuehren der Skripte
+import copy
 from readXML import PSt, DSt
 from MatchingAlgFu import matching
 from StabilityTest import stabilityCheck
+from PATest import AllPAinErg
+from MatchingStat import highestPref
 
 print(PSt)
 print(DSt)
+
+# fuer Statistik / zur Ueberpruefung der hoechsten Prioritaeten, da Liste PSt sich waehrend der Ausfuehrung des Alg. veraendert
+StatisticPSt = copy.deepcopy(PSt)
 
 #Seminarliste mit Anzahl der Plaetze
 Sem = [5,2,2]
@@ -17,3 +23,11 @@ print(f)
 # Test auf stabiles Matching durchfuehren
 g = stabilityCheck(f)
 print(g)
+
+# PA ueberpruefen
+h = AllPAinErg(f, DSt)
+print(h)
+
+# hoechste Prioritaeten ueberpruefen
+j = highestPref(f, StatisticPSt)
+print(j)
